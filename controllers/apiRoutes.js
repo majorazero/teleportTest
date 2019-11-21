c1da4fff13192c826ffdaab226f4742abedc0182
Dan Hsu <daniel.cw.hsu@gmail.com>
2019-11-20 16:07:30 -0800

    fixed minor bug

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -47,13 +47,20 @@ module.exports = (app) => {
   })
 
   app.get('/products/cheapestByType', (request, response) => {
-    db.ProductType.findAll().then((data) => {
+    db.ProductType.findAll({
+      include: [
+        {
+          model: db.Product,
+          include: [{model: db.Company}, {model: db.ProductType}]
+        }
+      ],
+    }).then((data) => {
       let arr = [];
 
       data.map((type) => {
-        let prices = type.products.map((product) => product.price);
+        let prices = type.Products.map((product) => product.price);
         let min    = Math.min(...prices);
-        let result = type.products.filter((item) => item.price === min);
+        let result = type.Products.filter((item) => item.price == min);
         arr.push(result[0]);
       })
 
@@ -62,24 +69,36 @@ module.exports = (app) => {
   })
 
   app.get('/products/companyCheap', (request, response) => {
-    db.Company.findAll().then((data) => {
+    db.Company.findAll({
+      include: [
+        {
+          model: db.Product
+        }
+      ],
+    }).then((data) => {
       let array = data.map((company) => {
-        let products = company.products.map((product) => product.price);
+        let products = company.Products.map((product) => product.price);
         let price = Math.min(...products);
         return {name: company.name, price: price}
       });
 
       let result = array.sort((a, b) => a.price - b.price);
-      res.json(result);
+      response.json(result);
     })
   })
 
 
-  app.get('products/freeShipping/:total', (request, response) => {
-    db.Product.where({shipping_threshold: {
-      [Op.lt]: request.params.total
-    }}).then((data) => {
-      response.json(data)
+  app.get('/products/type/:type', (request, response) => {
+    db.ProductType.findOne({
+      where: { name: request.params.type },
+      include: [
+        {
+          model: db.Product,
+          include: [{model: db.Company}, {model: db.ProductType}]
+        }
+      ],
+    }).then((data) => {
+      response.json(data.Products);
     })
   })
 };
